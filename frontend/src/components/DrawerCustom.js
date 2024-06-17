1e3f6411041aa3b9c10d2115369248d3b2d23c6d
Soham908 <sohampalekar908@gmail.com>
2024-06-17 15:16:52 +0530

    made the drawer responsive

--- frontend/src/components/DrawerCustom.js
+++ frontend/src/components/DrawerCustom.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import {
   Drawer,
   List,
@@ -7,24 +7,47 @@ import {
   Divider,
   ListItemButton,
   ListItemIcon,
+  useTheme,
+  useMediaQuery,
+  IconButton,
+  Typography,
 } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
 import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
 import LoginIcon from "@mui/icons-material/Login"
+import MenuIcon from "@mui/icons-material/Menu"
 import { useNavigate } from "react-router-dom";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle"
+import { UserContext } from "../App";
 
 const DrawerCustom = () => {
 
   const navigate = useNavigate()
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'));
+  const { userData } = useContext(UserContext)
+
+  const toggleDrawer = () => {
+    setOpenDrawer(!openDrawer);
+  };
 
   return (
+    <>
+     {isSmallScreen ? (
+        <IconButton onClick={toggleDrawer} sx={{ color: 'white', position: 'fixed', top: 10, left: 10 }}>
+          <MenuIcon />
+        </IconButton>
+      ) : null}
     <Drawer
-      variant="permanent"
+    variant={isSmallScreen ? 'temporary' : 'permanent'}
+    open={openDrawer}
+    onClose={toggleDrawer}
       sx={{
-        width: "17%",
+        width: {xs: "60%",sm: "30%", md: "25%", lg: "16%"},
         flexShrink: 0,
         [`& .MuiDrawer-paper`]: {
-          width: "17%",
+          width: {xs: "60%",sm: "30%", md: "25%", lg: "16%"},
           boxSizing: "border-box",
           backgroundColor: "#f4f4f4",
           padding: "0.5%",
@@ -33,13 +56,18 @@ const DrawerCustom = () => {
         },
       }}
     >
-      <Toolbar />
-      <Divider />
+      <Toolbar>
+          <AccountCircleIcon sx={{ width: 33, height: 33, color: 'wheat' }}/>
+          <Typography sx={{ marginLeft: 1, fontSize: 22 }}>
+            {userData?.username}
+          </Typography>
+        </Toolbar>
+      <Divider sx={{ backgroundColor: 'wheat' }}/>
       <List>
         {[
-          { text: "HomePage", icon: <HomeIcon />, onClick: () => {navigate("/")} },
-          { text: "Transaction History", icon: <AccountBalanceWalletIcon />, onClick: () => {navigate("/transactions")} },
-          { text: "Login", icon: <LoginIcon />, onClick: () => {navigate("/login")}}
+          { text: "HomePage", icon: <HomeIcon />, onClick: () => {navigate("/"); toggleDrawer()} },
+          { text: "Transaction History", icon: <AccountBalanceWalletIcon />, onClick: () => {navigate("/transactions"); toggleDrawer()} },
+          { text: "Login", icon: <LoginIcon />, onClick: () => {navigate("/login"); toggleDrawer()}}
         ].map((item, index) => (
           <ListItemButton
             key={item.text}
@@ -63,6 +91,8 @@ const DrawerCustom = () => {
         ))}
       </List>
     </Drawer>
+
+    </>
   );
 };
 
